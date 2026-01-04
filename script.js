90689bc01fef668a1ec9bf557a0f048de9584df4
Tanmay Wagh <162305300+TechExplorer15@users.noreply.github.com>
2026-01-04 17:15:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const inputs = document.querySelectorAll('.code');
 
+    // Initial focus (required by test)
     inputs[0].focus();
 
     inputs.forEach((input, index) => {
       input.addEventListener('keydown', (e) => {
         if (e.key === 'Backspace') {
           if (input.value === '' && index > 0) {
-            inputs[index - 1].value = '';
             inputs[index - 1].focus();
           } else {
             input.value = '';
@@ -15,7 +15,7 @@ const inputs = document.querySelectorAll('.code');
       });
 
       input.addEventListener('input', () => {
-        if (input.value.length === 1 && index < inputs.length - 1) {
+        if (input.value && index < inputs.length - 1) {
           inputs[index + 1].focus();
         }
       });
